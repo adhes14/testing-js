123fad015e04603223c7f4be35ce29a3997dba77
adhes14 <adhemarduran@gmail.com>
2023-01-21 22:11:58 -0400

    assertion or matchers examples

---
+++ demos/src/04-assertions.test.js
@@ -0,0 +1,30 @@
+test('test obj', () => {
+  const data = { name: 'nico' };
+  data.lastname = 'duran';
+  expect(data).toEqual({ name: 'nico', lastname: 'duran' });
+});
+
+test('null', () => {
+  const data = null;
+  expect(data).toBeNull();
+  expect(data).toBeDefined();
+  expect(data).not.toBeUndefined();
+});
+
+test('booleans', () => {
+  expect(true).toEqual(true);
+  expect(false).toEqual(false);
+
+  expect(0).toBeFalsy();
+  expect('').toBeFalsy();
+  expect(false).toBeFalsy();
+});
+
+test('string', () => {
+  expect('Christoph').toMatch(/stop/);
+});
+
+test('list / arrays', () => {
+  const numbers = [1, 2, 3, 4];
+  expect(numbers).toContain(3);
+});
