a83764202673ed2b22c7ca6ade4a2685b0a39048
adhes14 <adhemarduran@gmail.com>
2023-01-29 11:46:33 -0400

    create a simple ui test

---
+++ webapp/tests/example.spec.js
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+
+test('basic test', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  const title = page.locator('.navbar__inner .navbar__title');
+  await expect(title).toHaveText('Playwright');
+});
+
+test('has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
